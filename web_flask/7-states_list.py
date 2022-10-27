18a322db3fc3d8bb14951fecdc214ee23e3ef1c7
Aritesoma <99509115+Aritesoma@users.noreply.github.com>
2022-10-27 03:56:48 -0700

    web flask tasks

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
+"""
+from flask import Flask
+from flask import render_template
+from models import *
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
